5f73b40a609a445a3cf55c5e0609318fb77efe1a
ejilay <ivbaturin@gmail.com>
2018-01-22 18:15:59 +0300

    рабочие роуты

--- src/stream-api/main.go
+++ src/stream-api/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 
 	streamHandler "handlers/v1/streams"
 )
 
 func main() {
 	router := echo.New()
+	router.Pre(middleware.RemoveTrailingSlash())
 
-	v1 := router.Group("/api/v1")
+	v1 := router.Group("api/v1")
 
 	streams := v1.Group("streams")
 
@@ -20,4 +22,6 @@ func main() {
 	streams.POST("/", streamHandler.Create)
 
 	streams.PATCH("/:id", streamHandler.Update)
+
+	router.Logger.Fatal(router.Start("0.0.0.0:1323"))
 }
